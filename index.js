e516bcbf808d0cd64bd4d9ad25556126301ca0e3
gonber <goncalobernardo@gmail.com>
2019-12-09 20:34:11 +1100

    uses promises instead of callbacks
    BREAKING CHANGE

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-/* eslint-disable brace-style, camelcase, semi */
 /* global R5 */
 
-require('dotenv').config();
 module.exports = Rabbitmq;
 
 if (!global.R5) {
@@ -10,6 +8,8 @@ if (!global.R5) {
   };
 }
 
+const amqp = require('amqplib');
+
 // Constructors
 
 function Rabbitmq (host, user, pass, vhost = 'development') {
@@ -17,64 +17,89 @@ function Rabbitmq (host, user, pass, vhost = 'development') {
   this.user = user;
   this.pass = pass;
   this.vhost = vhost;
+  this.connect_retries = 0;
+  this.error_timeout = 10000;
+  this.consumers = [];
 }
 
 // Public Methods
 
-Rabbitmq.prototype.connect = function (config, retry_after_time = 5, callback = function () {}) {
-  let _this = this;
-  this.config = config;
+Rabbitmq.prototype = {
+  connect: async function (config) {
+    this.config = config;
+    const url = `amqp://${this.user}:${this.pass}@${this.host}/${this.vhost}`;
+    try {
+      this.conn = await amqp.connect(url);
+    }
+    catch (err) {
+      if (this.connect_retries++ < 10) {
+        R5.out.error(`RabbitMQ connecting (retrying [${this.connect_retries}]): ${err.code}`);
+        await delay(this.error_timeout * this.connect_retries);
+        return this.connect(config);
+      }
+      R5.out.error(`RabbitMQ connecting: ${err.stack}`);
+      throw err;
+    }
+    this.ch = await this.conn.createChannel();
+    await this.ch.assertExchange(config.exchange_name, 'topic', { durable: false });
+    R5.out.log(`Connected to RabbitMQ (queue: ${config.queue_name})`);
+    for (const consumer of this.consumers) {
+      await this.bind(consumer, true);
+    }
+    this.connect_retries = 0;
+    const _this = this;
+    this.conn.on('close', async function (err) {
+      if (err) {
+        R5.out.error(`[AMQP] reconnecting on close`);
+        return _this.connect(config);
+      }
+    });
+  },
 
-  _this.amqp = require('amqplib/callback_api');
-  _this.amqp.connect(`amqp://${_this.user}:${_this.pass}@${_this.host}/${_this.vhost}`, function (err, conn) {
-    if (err) { throw new Error(err.message); }
-    _this.conn = conn;
+  disconnect: async function () {
+    await this.ch.close();
+    await this.conn.close();
+  },
 
-    _this.conn.createChannel(function (err, ch) {
-      if (err) { throw new Error(err.message); }
-      retry_after_time = 5;
+  ack: function (msg) {
+    this.ch.ack(msg);
+  },
 
-      _this.ch = ch;
-      _this.ch.assertExchange(config.exchange_name, 'topic', { durable: false });
-      R5.out.log(`Connected to RabbitMQ (queue: ${config.queue_name})`);
-      return callback();
+  // eslint-disable-next-line no-unused-vars
+  bind: async function (callback = async (msg, message) => {}, reconnecting = false) {
+    await this.ch.assertQueue(this.config.queue_name, { durable: true });
+    await this.ch.prefetch(1);
+
+    R5.out.log(`Waiting for messages from ${this.config.message_type}..`);
+    await this.ch.consume(this.config.queue_name, function (msg) {
+      let message = parse_json(msg.content.toString());
+      return callback(msg, message);
+    }, { noAck: false });
+    if (!reconnecting) {
+      this.consumers.push(callback);
+    }
+  },
+
+  get: async function () {
+    await this.ch.assertQueue(this.config.queue_name, { durable: true });
+    const msg = await this.ch.get(this.config.queue_name, { noAck: false });
+    let message;
+    if (msg) {
+      message = parse_json(msg.content.toString());
+    }
+    return { msg, message };
+  },
+
+  send: async function (message) {
+    let message_string = JSON.stringify(message);
+
+    await this.ch.assertQueue(this.config.queue_name, { durable: true });
+    await this.ch.sendToQueue(this.config.queue_name, Buffer.from(message_string, 'utf8'), {
+      persistent: true
     });
 
-    _this.conn.on('close', function () {
-      R5.out.error(`[AMQP] reconnecting on close in ${retry_after_time} seconds`);
-      retry_after_time = retry_after_time + 5;
-      setTimeout(function () {
-        _this.connect(_this.config, retry_after_time);
-      }, (retry_after_time - 5) * 1000);
-    });
-  });
-}
-
-Rabbitmq.prototype.ack = function (msg) {
-  this.ch.ack(msg);
-}
-
-Rabbitmq.prototype.bind = function (callback) {
-  this.ch.assertQueue(this.config.queue_name, { durable: true });
-  this.ch.prefetch(1);
-
-  R5.out.log(`Waiting for messages from ${this.config.message_type}..`);
-  this.ch.consume(this.config.queue_name, function (msg) {
-    let message = parse_json(msg.content.toString());
-    callback(msg, message);
-  }, { noAck: false });
-};
-
-Rabbitmq.prototype.send = function (message, callback = function () {}) {
-  let message_string = JSON.stringify(message);
-
-  this.ch.assertQueue(this.config.queue_name, { durable: true });
-  this.ch.sendToQueue(this.config.queue_name, Buffer.from(message_string, 'utf8'), {
-    persistent: true
-  });
-
-  R5.out.log(`SEND ${message.category}:${message.type}`);
-  return callback();
+    R5.out.log(`SEND ${message.category}:${message.type}`);
+  },
 };
 
 // Private Methods
@@ -82,7 +107,8 @@ Rabbitmq.prototype.send = function (message, callback = function () {}) {
 function json_is_valid (json_str) {
   try {
     return (JSON.parse(json_str) && !!json_str);
-  } catch (e) {
+  }
+  catch (e) {
     return false;
   }
 }
@@ -91,11 +117,15 @@ function parse_json (str) {
   let parsed_result = null;
 
   if (json_is_valid(str)) {
-    parsed_result = JSON.parse(str)
+    parsed_result = JSON.parse(str);
   }
   else {
     R5.out.error(`JSON is invalid: ${str}`);
   }
 
   return parsed_result;
+}
+
+function delay (ms) {
+  return new Promise((res) => setTimeout(res, ms));
 }
